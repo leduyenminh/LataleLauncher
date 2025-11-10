f5cb4f1fb48e8160266a4945a5fab8ec9eef5546
leduyenminh <42405761+leduyenminh@users.noreply.github.com>
2025-11-10 08:44:54 +0000

    Latale - update gateway config

--- APIGateway/src/main/java/com/apigateway/APIGatewayApplication.java
+++ APIGateway/src/main/java/com/apigateway/APIGatewayApplication.java
@@ -1,8 +1,9 @@
 package com.apigateway;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
-@EnableCaching
-@EnableAsync
 public class APIGatewayApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(APIGatewayApplication.class, args);
